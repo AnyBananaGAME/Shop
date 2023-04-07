85f177a4c166ad751ffb1ea914e1f4c1501d0b73
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-04-07 12:52:03 +0200

    gay

--- src/bonana/EventListener.php
+++ src/bonana/EventListener.php
@@ -10,26 +10,16 @@ use muqsit\invmenu\InvMenuHandler;
 class EventListener implements Listener {
 	private Main $plugin;
 
-    public function __construct(Main $plugin) {
+        public function __construct(Main $plugin) {
 		$this->plugin = $plugin;
 	}
 
-    // THIS IS FOR TESTING ONLY
-    // THIS IS FOR TESTING ONLY
-    // THIS IS FOR TESTING ONLY
-    // THIS IS FOR TESTING ONLY
-    // THIS IS FOR TESTING ONLY
-    // THIS IS FOR TESTING ONLY
-
 	public function onBreak(BlockBreakEvent $event): void {
 		$player = $event->getPlayer();
-        $server = $event->getPlayer()->getServer();
-        $blockID = $event->getBlock()->getId();
-        $blockMETA = $event->getBlock()->getMeta();
+	        $server = $event->getPlayer()->getServer();
+        	$blockID = $event->getBlock()->getId();
+	        $blockMETA = $event->getBlock()->getMeta();
 
-        $player->sendMessage("id " . $blockID . "\nmeta ". $blockMETA);
-       //  $this->plugin->getServer()->getCommandMap()->dispatch($player, "shop");
+        	$player->sendMessage("id " . $blockID . "\nmeta ". $blockMETA);
     }
-
-
 }
