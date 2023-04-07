c4f7f7952ae07ede3c2fc75da491418fec4b9b25
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-04-07 12:55:39 +0200

    Update XPCommand.php

--- src/bonana/commands/XPCommand.php
+++ src/bonana/commands/XPCommand.php
@@ -11,34 +11,29 @@ use function in_array;
 use function is_numeric;
 use function strtolower;
 
-class XPCommand extends Command implements PluginOwned
-{
-
+class XPCommand extends Command implements PluginOwned {
     use PluginOwnedTrait;
 
-    public function __construct(Main $plugin)
-    {
+    public function __construct(Main $plugin) {
         $this->owningPlugin = $plugin;
         parent::__construct("xp", "See how much xp a player has.", "/xp <player>");
         $this->setPermission("xp.command");
         $this->setDescription("See how much xp a player has.");
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args): bool
-    {
-        if(!isset($args[0])) {
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if (!isset($args[0])) {
             $player = $this->getOwningPlugin()->getServer()->getPlayerExact($sender->getName());
             $username = $player->getName();
             $xp = $player->getXpManager()->getCurrentTotalXp();
             $sender->sendMessage("You have $xp xp");
             return false;
         }
+
         $player = $this->getOwningPlugin()->getServer()->getPlayerExact($args[0]);
         $username = $player->getName();
         $xp = $player->getXpManager()->getCurrentTotalXp();
         $sender->sendMessage("$username has $xp xp");
         return true;
     }
-
-
 }
